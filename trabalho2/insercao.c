0270c65630c5ee48bdfb332ba2427355f56c4f4f
Mapnbp <165668357+Mapnbp@users.noreply.github.com>
2024-05-20 17:53:17 -0300

    Update insercao.c

--- trabalho2/insercao.c
+++ trabalho2/insercao.c
@@ -1,9 +1,7 @@
 #include "binFile.h"
 #include "index.h"
 
-
-
-//O indice eh guardado em RAM, n em disco, da uma olhada dps no meu codigo
+// O indice eh guardado em RAM, n em disco, da uma olhada dps no meu codigo (edicao a ser feita)
 void atualizarIndice(FILE *arquivoIndice, int id, long posicao) {
     INDEX novoIndice;
     novoIndice->data->id = id;
@@ -14,46 +12,50 @@ void atualizarIndice(FILE *arquivoIndice, int id, long posicao) {
 
 }
 
+void escreverRegistro(FILE *arquivoDados, PLAYER *newPlayer, long posicao, FILE *arquivoIndice) {
+    fwrite(&newPlayer->id, sizeof(int), 1, arquivoDados);
+    fwrite(&newPlayer->idade, sizeof(int), 1, arquivoDados);
+    fwrite(newPlayer->nome, sizeof(char), newPlayer->nomeLen, arquivoDados);
+    fwrite(newPlayer->pais, sizeof(char), newPlayer->paisLen, arquivoDados);
+    fwrite(newPlayer->clube, sizeof(char), newPlayer->clubeLen, arquivoDados);
 
-//A insercao  n eh so no final, tem q usar best fit, procurar pelos logicamente removidos, tem nos slides
-//sobre reuso de espaco
+}
+
 void insertion(char *arqDados, char *arqInd, int n) {
-
-    arquivoDados = fopen(arqDados, "ab");
-    arquivoIndice = fopen(arqInd, "ab");//Msm coisa, ta em memoria principal
+    FILE *arquivoDados, *arquivoIndice;
+    arquivoDados = fopen(arqDados, "r+b");
+    arquivoIndice = fopen(arqInd, "r+b");//Msm coisa, ta em memoria principal
 
     if (arquivoDados == NULL || arquivoIndice == NULL) {
         printf("Falha no processamento do arquivo.\n");
         return EXIT_FAILURE;
     }
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         PLAYER *newPlayer;
+        char str1[32], str2[32], str3[32];
         newPlayer = playerInit();
+        newPlayer->nome, newPlayer->pais, newPlayer->clube
 
-        //Dentro das funcoes fornecidas tem uma que le string entre aspas, usa ela q fica mais legivel
-        scanf("%d, %d, \"%[^\"]\", \"%[^\"]\", \"%[^\"]\"", &newPlayer->id, &newPlayer->idade, newPlayer->nome, newPlayer->pais, newPlayer->clube);
+        scanf("%d, %d, %s, %s, %s", &newPlayer->id, &newPlayer->idade, str1, str2, str3);
+        scan_quote_string(str1);
+        scan_quote_string(str2);
+        scan_quote_string(str3);
+        newPlayer->nome = strdup(str1);
+        newPlayer->pais = strdup(str2);
+        newPlayer->clube = strdup(str3);
+        newPlayer->nomeLen = strlen(newPlayer->nome) + 1; // +1 do caractere nulo
+        newPlayer->paisLen = strlen(newPlayer->pais) + 1;
+        newPlayer->clubeLen = strlen(newPlayer->clube) + 1;
 
+        // encontrar o registro lógico removido 
+        // registro removido e marcar o lixo com '$'
 
+        // caso nao tenha registros removidos
+        } else {
 
-
-        int8_t tempByte = '0';
-        int64_t temp8bytes = -1;
-        fwrite(&tempByte, 1, 1, arqDados);
-        int32_t regSize = playerTamanho(newPlayer);
-        
-        fwrite(&regSize,4, 1, arquivoDados);
-        fwrite(&temp8bytes, 8, 1, arquivoDados); 
-        fwrite(&(newPlayer->id), 4, 1, arquivoDados);
-        fwrite(&(newPlayer->idade), 4, 1, arquivoDados);
-        fwrite((newPlayer->nome), 1,player->nomeLen, arquivoDados);
-        fwrite((newPlayer->pais), 1,player->paisLen, arquivoDados);
-        fwrite((newPlayer->clube), 1,player->clubeLen, arquivoDados); 
-
-        atualizarIndice(arquivoIndice, newPlayer->id, ftell(arquivoDados));
-
+        }
     }
 
     fclose(arquivoDados);
-    fclose(arquivoIndice);
 }
