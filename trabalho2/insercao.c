647cea7a963f6978bc3f7986cf14fc22a9a61e33
Mapnbp <165668357+Mapnbp@users.noreply.github.com>
2024-05-26 13:42:57 -0300

    Update insercao.c

--- trabalho2/insercao.c
+++ trabalho2/insercao.c
@@ -1,15 +1,22 @@
 #include "binFile.h"
 #include "index.h"
 
-// O indice eh guardado em RAM, n em disco, da uma olhada dps no meu codigo (edicao a ser feita)
-void atualizarIndice(FILE *arquivoIndice, int id, long posicao) {
-    INDEX novoIndice;
-    novoIndice->data->id = id;
-    novoIndice->data->offset = posicao;
+void indexAtualizarOuInserir(INDEX* index, int id, int offset) {
+    int indToUpdate = indexSearchPosition(index, id);
+    if (indToUpdate != '$' && index->array[indToUpdate].id == id) {
 
-    fseek(arquivoIndice, 0, SEEK_END);
-    fwrite(&novoIndice, sizeof(INDEX), 1, arquivoIndice);
+        index->array[indToUpdate].offset = offset;
+    } else {
 
+        shiftRight(index, indToUpdate + 1);
+        index->array[indToUpdate + 1].id = id;
+        index->array[indToUpdate + 1].offset = offset;
+        index->nReg++;
+        if (index->nReg == index->arrLen) {
+            index->arrLen *= 2;
+            index->array = (struct data*) realloc(index->array, index->arrLen * sizeof(struct data));
+        }
+    }
 }
 
 void escreverRegistro(FILE *arquivoDados, PLAYER *newPlayer, long posicao, FILE *arquivoIndice) {
@@ -19,12 +26,15 @@ void escreverRegistro(FILE *arquivoDados, PLAYER *newPlayer, long posicao, FILE 
     fwrite(newPlayer->pais, sizeof(char), newPlayer->paisLen, arquivoDados);
     fwrite(newPlayer->clube, sizeof(char), newPlayer->clubeLen, arquivoDados);
 
+        // Insere ou atualiza o índice
+    fseek(arquivoDados, posicao, SEEK_SET);
+    indexAtualizarOuInserir(arquivoIndice, newPlayer->id, posicao);
 }
 
-void insertion(char *arqDados, char *arqInd, int n) {
-    FILE *arquivoDados, *arquivoIndice;
+void insertion(char *arqDados, INDEX *arqInd, int n) {
+    FILE *arquivoDados;
+    INDEX arquivoIndice = arqInd;
     arquivoDados = fopen(arqDados, "r+b");
-    arquivoIndice = fopen(arqInd, "r+b");//Msm coisa, ta em memoria principal
 
     if (arquivoDados == NULL || arquivoIndice == NULL) {
         printf("Falha no processamento do arquivo.\n");
@@ -48,13 +58,10 @@ void insertion(char *arqDados, char *arqInd, int n) {
         newPlayer->paisLen = strlen(newPlayer->pais) + 1;
         newPlayer->clubeLen = strlen(newPlayer->clube) + 1;
 
-        // encontrar o registro lógico removido 
-        // registro removido e marcar o lixo com '$'
+        long posicao = ftell(arquivoDados); // Salva a posição atual no arquivo de dados
+        escreverRegistro(arquivoDados, newPlayer, posicao, arquivoIndice);
+        free(newPlayer);
 
-        // caso nao tenha registros removidos
-        } else {
-
-        }
     }
 
     fclose(arquivoDados);
